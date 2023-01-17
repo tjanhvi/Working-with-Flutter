70772cc428482f251794205679d70661a3f6c898
JANHVI TIWARI <tjanhvi2003@outlook.com>
2023-01-17 11:22:01 +0530

    Added lines

--- Flutter/my_app/lib/main.dart
+++ Flutter/my_app/lib/main.dart
@@ -3,86 +3,121 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(MaterialApp(
-    home: Scaffold(
-      body: Center(
-        child: Container(
-          padding: EdgeInsets.all(40), 
-          color: Colors.redAccent,
-          width: 200,
-          height: 200,
-          child: Container(
-            color: Colors.blueAccent,
-            padding: EdgeInsets.all(35),
-            child: Container(
-              color: Colors.greenAccent,
-              
-            ),
-          ),
-        ),
-      ),
-    )
-  ));
+  runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return new MaterialApp(
-      title: "My App",
-      home: new HomePage(),
-    );    
-  }
-}
-
-class HomePage extends StatefulWidget {
-  @override
-  _HomePageState createState() => _HomePageState();
-}
-
-
-class _HomePageState extends State<HomePage> {
-
-  String mytext = "Hello World";
-
-  void _changeText(){
-    setState((){
-      if(mytext.startsWith("H")){
-        mytext = "Welcome to my app";
-      }
-      else{
-        mytext = "Hello World";
-      }
-    });
-  }
-
-  Widget _bodyWidget() {
-    return new Container(
-      padding: const EdgeInsets.all(8.0),
-      child: new Center(
-        child: new Column(
-          children: <Widget>[
-            new Text(mytext),
-            new ElevatedButton(
-              child: new Text("Click"),
-              onPressed: _changeText,
-            )
-          ],
-        ), 
+    return MaterialApp(
+      home: Scaffold(
+        backgroundColor: Colors.white,
+        body: SafeArea(
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: <Widget>[               
+              Container(
+                width: 90.0,
+                color: Colors.red,                            
+              ),                 
+              Container(
+                width: 90.0,                    
+                color: Colors.blue,
+              ),
+              Container(
+                width: 90.0,
+                color: Colors.green,
+              ),
+            ],
+          ),
+        ),
       ),
     );
   }
-
-  @override
-  Widget build(BuildContext context) {
-    return new Scaffold(
-      appBar: new AppBar(
-        title: Text("Home Page"),
-      ),
-      body: _bodyWidget(),
-    );
-  }
 }
+// import 'package:flutter/material.dart';
+
+// void main() {
+//   runApp(MaterialApp(
+//     home: Scaffold(
+//       body: Center(
+//         child: Container(
+//           padding: EdgeInsets.all(40), 
+//           color: Colors.redAccent,
+//           width: 200,
+//           height: 200,
+//           child: Container(
+//             color: Colors.blueAccent,
+//             padding: EdgeInsets.all(35),
+//             child: Container(
+//               color: Colors.greenAccent,
+              
+//             ),
+//           ),
+//         ),
+//       ),
+//     )
+//   ));
+// }
+
+// class MyApp extends StatelessWidget {
+//   @override
+//   Widget build(BuildContext context) {
+//     return new MaterialApp(
+//       title: "My App",
+//       home: new HomePage(),
+//     );    
+//   }
+// }
+
+// class HomePage extends StatefulWidget {
+//   @override
+//   _HomePageState createState() => _HomePageState();
+// }
+
+
+// class _HomePageState extends State<HomePage> {
+
+//   String mytext = "Hello World";
+
+//   void _changeText(){
+//     setState((){
+//       if(mytext.startsWith("H")){
+//         mytext = "Welcome to my app";
+//       }
+//       else{
+//         mytext = "Hello World";
+//       }
+//     });
+//   }
+
+//   Widget _bodyWidget() {
+//     return new Container(
+//       padding: const EdgeInsets.all(8.0),
+//       child: new Center(
+//         child: new Column(
+//           children: <Widget>[
+//             new Text(mytext),
+//             new ElevatedButton(
+//               child: new Text("Click"),
+//               onPressed: _changeText,
+//             )
+//           ],
+//         ), 
+//       ),
+//     );
+//   }
+
+//   @override
+//   Widget build(BuildContext context) {
+//     return new Scaffold(
+//       appBar: new AppBar(
+//         title: Text("Home Page"),
+//       ),
+//       body: _bodyWidget(),
+//     );
+//   }
+// }
 // void main() {
 //   runApp(MaterialApp(
 //     home: Scaffold(
